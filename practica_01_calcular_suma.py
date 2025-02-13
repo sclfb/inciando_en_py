b32db31257e2ad1fa47c65b74544f9af70608624
fernando <soycodeluisfb@gmail.com>
2025-02-12 20:43:37 -0500

    segunda prueba del commit

--- practica_01_calcular_suma.py
+++ practica_01_calcular_suma.py
@@ -3,7 +3,7 @@
 num_uno = float(input("ingrese por favor un numero: "))
 num_dos = float(input("ingrese por favor un segundo numero: "))
 
-# sumamos los dos numeros 
+# sumamos los dos numeros de una forma sencilla
 
 operacion_suma = num_uno + num_dos
 
