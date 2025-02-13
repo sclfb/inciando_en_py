b881f8ebd4e9c91a12c08388ef1e422c0cb0491c
fernando <soycodeluisfb@gmail.com>
2025-02-12 20:03:36 -0500

    primer commit: iniciando py con la suma de 2 numeros

---
+++ practica_01_calculadora.py
@@ -0,0 +1,12 @@
+#solicitamos que el usuario ingrese dos numeros
+
+num_uno = float(input("ingrese por favor un numero: "))
+num_dos = float(input("ingrese por favor un segundo numero: "))
+
+# sumamos los dos numeros 
+
+operacion_suma = num_uno + num_dos
+
+#mostramos el resultado
+
+print(operacion_suma)
